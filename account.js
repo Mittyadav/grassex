cd8592f7a678ec612f387f900981bdaff2bb0fb5
@mitt_ydv <146659530+Mittyadav@users.noreply.github.com>
2024-12-23 09:01:47 +0530

    Update account.js

--- account.js
+++ account.js
@@ -1,14 +1,16 @@
-export const accountLists = [
+const accountLists = [
     {
-      "username": "xxx",
-      "password": "xxx"
+        "username": "xxx",
+        "password": "xxx"
     },
     {
-      "username": "xxx",
-      "password": "xxx"
+        "username": "xxx",
+        "password": "xxx"
     },
     {
-      "username": "xxx",
-      "password": "xxx"
-    } // for more users just copy paste the form as many as your accounts
+        "username": "xxx",
+        "password": "xxx"
+    } // for more users just copy-paste the form as many as your accounts
 ];
+
+module.exports = accountLists;
